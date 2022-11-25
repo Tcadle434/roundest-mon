0f56d33aff4900c76904612feece50332b60fbba
Tcadle434 <thomascadle434@gmail.com>
2022-11-24 23:50:30 -0700

    testing

--- prisma/migrations/20221125022901_add_pokemon_reference/migration.sql
+++ prisma/migrations/20221125022901_add_pokemon_reference/migration.sql
@@ -16,8 +16,8 @@ ALTER TABLE `Vote` DROP COLUMN `votedAgainst`,
 -- CreateTable
 CREATE TABLE `Pokemon` (
     `id` INTEGER NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `spriteUrl` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL COLLATE utf8mb4_unicode_ci NOT NULL,
+    `spriteUrl` VARCHAR(191) NOT NULL COLLATE utf8mb4_unicode_ci NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
